9b89c7486a8abcbabe2c7cfbcdff98116b1ade3b
Jake Andrus <thejakeandrus@gmail.com>
2020-04-29 11:12:59 -0600

    generate working

--- generate.py
+++ generate.py
@@ -4,15 +4,15 @@ from io_handler import IOHandler
 
 
 class Generator():
-		def __init__(self, io):
-				self.io = io
+    def __init__(self, io):
+        self.io = io
     def generate(self, w, path1, path2):
         print('weights:', w, 'path to img:', path1, " path to folder:", path2)
         impr_gen = tf.keras.models.load_model('models/photo-to-impr-generator-00001.h5')
-				imgs = self.io.imgs_to_np(path1)
-				X_out = impr_gen.predict(imgs)
-				X_out = (X_out+1)*127.5
-
-				self.io.save_imgs(path2, X_out)
+        #impr_gen = tf.keras.models.load_model('models/g_model_AtoB_000001.h5')
+        imgs = self.io.imgs_to_np(path1)
+        X_out = impr_gen.predict(imgs)
+        X_out = (X_out+1)*127.5
+        self.io.save_imgs(path2, X_out)
 
 
--- impressigan.py
+++ impressigan.py
@@ -20,26 +20,29 @@ style = style_from_dict({ #taken from the codeburst example and tweaked
     Token.Question: '',
 })
 
+WORKDIR = ''
 
 class PictureValidator(Validator): #checking if the folder has at least 1000 pics in it
     def validate(self, document):
-        valid =  os.path.isabs(document.text)
+        valid = True# os.path.isabs(document.text)
         if not valid:
             raise ValidationError(
                 message='Please enter the directory from the root:',
                 cursor_position=len(document.text))  # Move cursor to end'
-        exists = os.path.exists(document.text) 
+        global WORKDIR
+        wdir = WORKDIR + '/' if WORKDIR else WORKDIR
+        exists = os.path.exists(wdir + document.text) 
         if not exists:
             raise ValidationError(
             message='The file path does not exist! Please enter a valid path:',
             cursor_position=len(document.text))  # Move cursor to end'
-        is_folder = os.path.isdir(document.text) 
+        is_folder = os.path.isdir(wdir + document.text) 
         if not is_folder:
             raise ValidationError(
             message='The file path does not lead to a folder! Please enter a valid path:',
             cursor_position=len(document.text))  # Move cursor to end'
         has_1000 = False
-        contents = len(os.listdir(document.text))
+        contents = len(os.listdir(wdir + document.text))
         if contents < 10:
             raise ValidationError(
             message='There is not enough files to properly train in this folder! Please try again:',
@@ -48,17 +51,19 @@ class PictureValidator(Validator): #checking if the folder has at least 1000 pic
 
 class PathValidator(Validator): #checking if is folder
     def validate(self, document):
-        valid =  os.path.isabs(document.text)
+        valid = True# os.path.isabs(document.text)
         if not valid:
             raise ValidationError(
                 message='Please enter the directory from the root:',
                 cursor_position=len(document.text))  # Move cursor to end'
-        exists = os.path.exists(document.text) 
+        global WORKDIR
+        wdir = WORKDIR + '/' if WORKDIR else WORKDIR
+        exists = os.path.exists(wdir + document.text) 
         if not exists:
             raise ValidationError(
             message='The file path does not exist! Please enter a valid path:',
             cursor_position=len(document.text))  # Move cursor to end'
-        is_folder = os.path.isdir(document.text) 
+        is_folder = os.path.isdir(wdir + document.text) 
         if not is_folder:
             raise ValidationError(
             message='The file path does not lead to a folder! Please enter a valid path:',
@@ -69,24 +74,30 @@ class PathValidator(Validator): #checking if is folder
 
 class FileValidator(Validator): #checking if is file and an image
     def validate(self, document):
-        valid =  os.path.isabs(document.text)
+        valid = True #os.path.isabs(document.text)
         if not valid:
             raise ValidationError(
                 message='Please enter the directory from the root:',
                 cursor_position=len(document.text))  # Move cursor to end'
-        exists = os.path.exists(document.text) 
+        global WORKDIR
+        wdir = WORKDIR + '/' if WORKDIR else WORKDIR
+        exists = os.path.exists(wdir + document.text) 
         if not exists:
             raise ValidationError(
             message='The file path does not exist! Please enter a valid path:',
             cursor_position=len(document.text))  # Move cursor to end'
-        is_file = os.path.isfile(document.text) 
-        doc = os.path.basename(document.text)
-        f_type = doc.split('.')
-        is_img = True if f_type[1] == 'pdf' or f_type[1] == 'jpg' or f_type[1] == 'png' else False
-        if not (is_file and is_img):
-            raise ValidationError(
-            message='The file path does not lead to an image! Please enter a valid path:',
-            cursor_position=len(document.text))  # Move cursor to end'
+        
+        files = os.listdir(wdir + document.text)
+        for file in files:
+            file = wdir + document.text + '/' + file
+            is_file = os.path.isfile(file) 
+            doc = os.path.basename(file)
+            f_type = doc.split('.')
+            is_img = True if f_type[1] == 'pdf' or f_type[1] == 'jpg' or f_type[1] == 'png' else False
+            if not (is_file and is_img):
+                raise ValidationError(
+                message='The file path does not lead to an image! Please enter a valid path:',
+                cursor_position=len(document.text))  # Move cursor to end'
         
 
 
@@ -99,7 +110,10 @@ class App():
             'message': 'Please enter the your working directory!',
             'validate': PathValidator
         }])
-        self.io = IOHandler(answer['workdir'])
+        global WORKDIR
+        WORKDIR = answer['workdir']
+        print('now', WORKDIR)
+        self.io = IOHandler(WORKDIR)
         image_shape = (256, 256, 3)
         self.gen = Generator(self.io)
         self.trainer = Trainer(image_shape, self.io)
@@ -184,94 +198,3 @@ class App():
         self.trainer.train(answers['src images'], answers['src art'])
 
 app = App()
-
-
-# print('Welcome to ImpressiGAN!')
-
-# questions = [
-#     {
-#         'type': 'checkbox',
-#         'message': 'What do you want to do?',
-#         'name': 'actions',
-#         'choices': [
-#             Separator('= Actions ='),
-#             {
-#                 'name': 'Train a Model (this will take a while!)'
-#             },
-#             {
-#                 'name': 'Generate a Picture'
-#             },
-#             {
-#                 'name': 'See Cool Pics'
-#             },
-#         ],
-#         'validate': lambda answer: 'You must choose an option' \
-#             if len(answer) == 0 else True
-#     },
-#     {
-#         'type': 'input',
-#         'name': 'src image',
-#         'message': 'Please enter the file path to the image you want to convert from the root directory!',
-#         'validate': FileValidator
-#         'when': lambda answers: answers['actions'] == 'Train a Model (this will take a while!)'
-#     },
-#     {
-#         'type': 'list',
-#         'name': 'size',
-#         'message': 'What size do you need?',
-#         'choices': ['Large', 'Medium', 'Small'],
-#         'filter': lambda val: val.lower()
-#     },
-#     {
-#         'type': 'input',
-#         'name': 'quantity',
-#         'message': 'How many do you need?',
-#         'validate': NumberValidator,
-#         'filter': lambda val: int(val)
-#     },
-#     {
-#         'type': 'expand',
-#         'name': 'toppings',
-#         'message': 'What about the toppings?',
-#         'choices': [
-#             {
-#                 'key': 'p',
-#                 'name': 'Pepperoni and cheese',
-#                 'value': 'PepperoniCheese'
-#             },
-#             {
-#                 'key': 'a',
-#                 'name': 'All dressed',
-#                 'value': 'alldressed'
-#             },
-#             {
-#                 'key': 'w',
-#                 'name': 'Hawaiian',
-#                 'value': 'hawaiian'
-#             }
-#         ]
-#     },
-#     {
-#         'type': 'rawlist',
-#         'name': 'beverage',
-#         'message': 'You also get a free 2L beverage',
-#         'choices': ['Pepsi', '7up', 'Coke']
-#     },
-#     {
-#         'type': 'input',
-#         'name': 'comments',
-#         'message': 'Any comments on your purchase experience?',
-#         'default': 'Nope, all good!'
-#     },
-#     {
-#         'type': 'list',
-#         'name': 'prize',
-#         'message': 'For leaving a comment, you get a freebie',
-#         'choices': ['cake', 'fries'],
-#         'when': lambda answers: answers['comments'] != 'Nope, all good!'
-#     }
-# ]
-
-# answers = prompt(questions, style=style)
-# print('Order receipt:')
-# pprint(answers)
